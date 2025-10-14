6f682e039a3310f47982badf6d4495e46ab2b371
Felipe <kikoalbuquerque02@gmail.com>
2025-10-14 19:01:03 -0300

    New pages implemented for the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import Header from "./Components/header/Header.jsx";
-import Footer from "./Components/footer/Footer.jsx";
-import Home from "./Components/board/Home.jsx";
+import Header from "./Components/header/Header";
+import Footer from "./Components/footer/Footer";
+import { Outlet } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
 
 function App() {
+  const navigation = useNavigation();
   return (
     <>
       <Header />
-      <Home />
+      {navigation.state === "loading" ? (
+        <div className="flex items-center justify-center min-h-[852px]">
+          <span className="text-4xl font-semibold text-primary dark:text-light">
+            Loading...
+          </span>
+        </div>
+      ) : (
+        <Outlet />
+      )}
+
       <Footer />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import About from "./Components/About/About.jsx";
+import Contact from "./Components/Contact/Contact.jsx";
+import Login from "./Components/Login/Login.jsx";
+import Cart from "./Components/Cart/Cart.jsx";
+import Home from "./Components/Home/Home.jsx";
+import { productsLoader } from "./Components/Home/Home.jsx";
+import ErrorPage from "./Components/Error/ErrorPage.jsx";
+import { contactAction } from "./Components/Contact/Contact.jsx";
+import ProductDetail from "./Components/Home/products/ProductDetail.jsx";
 
-createRoot(document.getElementById('root')).render(
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} loader={productsLoader} />
+    <Route path="/home" element={<Home />} loader={productsLoader} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} action={contactAction} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/products/:productId" element={<ProductDetail />} />
+  </Route>
+);
+
+const appRouter = createBrowserRouter(routeDefinitions);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={appRouter} />
+    <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      draggable
+      pauseOnHover
+      theme={localStorage.getItem("theme") === "dark" ? "dark" : "light"}
+      transition={Bounce}
+    />
+  </StrictMode>
+);
